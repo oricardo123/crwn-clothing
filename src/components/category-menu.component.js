cd2bb93bfb6cd910c65c59d9e7cfacc1ad99abf9
Ricardo <ricardoalmeida312@gmail.com>
2023-04-18 00:30:06 +0100

    second part finished

---
+++ src/components/category-menu.component.js
@@ -0,0 +1,13 @@
+import CategoryItem from "./catergory-item/category-item.component";
+import "./category-menu.styles.scss";
+const CategoryMenu = ({ categories }) => {
+  return (
+    <div className="categories-container">
+      {categories.map((category) => {
+        return <CategoryItem key={category.id} category={category} />;
+      })}
+    </div>
+  );
+};
+
+export default CategoryMenu;
